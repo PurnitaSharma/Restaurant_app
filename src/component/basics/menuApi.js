624fa38d509c88d4f7fbea3713673ed3d1c826e7
PurnitaSharma <purnita08@gmail.com>
2023-07-19 22:42:36 +0530

    Upload

---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Maggie is a popular brand of instant noodles known for its quick preparation and unique flavors. It is a favorite among college students and busy individuals seeking a tasty and convenient meal.",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoda.jpg",
+      name: "allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Allu pakoda is a popular Indian snack made by deep-frying sliced potatoes coated in a spiced chickpea flour batter. Crispy and flavorful, it is commonly enjoyed with tea during rainy days or as an appetizer." , 
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Corn, a versatile grain, is enjoyed in various forms, including boiled, grilled, or roasted. It is a good source of fiber and nutrients. Corn can be used in salads, soups, tacos, and as a side dish, adding a sweet and crunchy element to the meal.",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Chola, also known as chana or chickpeas, is a legume commonly used in Indian cuisine. It is cooked with spices to create a delicious curry or added to salads and chaats. Chola is rich in protein, fiber, and essential minerals.",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Pizza is a globally beloved dish originating from Italy. It typically consists of a round dough base topped with tomato sauce, cheese, and various toppings. It can be customized to individual preferences and is baked until the crust is crispy and the cheese is melted.",
+    },
+    {
+      id: 6,
+      image: "../images/thali.jpg",
+      name: "Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "A thali is a traditional Indian meal served on a large plate, featuring a variety of dishes, including rice, bread, curries, dal, vegetables, yogurt, and pickles. It offers a balanced combination of flavors, textures, and nutrients, providing a satisfying and complete dining experience.",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        " Sweets, also known as desserts or mithai in India, are indulgent treats enjoyed after meals or during festive occasions. They come in various forms like gulab jamun, jalebi, barfi, and ladoo. Sweets are often made with ingredients like milk, sugar, nuts, and spices, creating a delightful and rich experience.",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma rice is a popular North Indian dish consisting of red kidney beans cooked in a thick tomato-based gravy, served with steamed rice. It is a hearty and comforting meal packed with protein, fiber, and flavors, often enjoyed with a side of yogurt or pickle.",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Samosa is a triangular-shaped pastry originating from India, filled with a savory mixture of spiced potatoes, peas, and sometimes meat. It is deep-fried until golden and crispy, making it a popular street food and appetizer. Samosas are often served with chutneys or sauces for added flavor.",
+    },
+  ];
+  
+  export default Menu;
